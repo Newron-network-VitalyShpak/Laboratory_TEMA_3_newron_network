56fcf432e33d1f4f6b590b98f639ef73010e6522
VitalyShpak9222 <vitakysp@gmail.com>
2024-12-04 12:44:17 +0300

    update 1 final

--- Laboratory_TEMA_3_newron_network/Laboratory_TEMA_3_newron_network.py
+++ Laboratory_TEMA_3_newron_network/Laboratory_TEMA_3_newron_network.py
@@ -14,22 +14,42 @@ def target_function(x):
     return torch.sin(x+2) * 3**x
 
 def metric(pred, target):
-    return (pred - target).abs().mean()
+    # return (pred - target).abs().mean()
+    return ((pred - target) ** 2).mean()
+
 
 class RegressionNet(torch.nn.Module):
     def __init__(self, hidden_size):
-        super().__init__()
+        super(RegressionNet, self).__init__()
         self.layer1 = torch.nn.Linear(1, hidden_size)
-        self.act1 = torch.nn.Sigmoid()
-        self.layer2 = torch.nn.Linear(hidden_size, 1)
-
+        self.act1 = torch.nn.ReLU() 
+        self.layer2 = torch.nn.Linear(hidden_size, int(hidden_size / 2)) 
+        self.act2 = torch.nn.ReLU()
+        self.output_layer = torch.nn.Linear(int(hidden_size / 2), 1)
+    
     def forward(self, x):
         z = self.layer1(x)
         z = self.act1(z)
         z = self.layer2(z)
-        return z
+        z = self.act2(z)
+        output = self.output_layer(z)
+        return output
 
-net = RegressionNet(128)  
+
+# class RegressionNet(torch.nn.Module):
+#     def __init__(self, hidden_size):
+#         super().__init__()
+#         self.layer1 = torch.nn.Linear(1, hidden_size)
+#         self.act1 = torch.nn.Sigmoid() # torch.nn.ReLU() 
+#         self.layer2 = torch.nn.Linear(hidden_size, 1)
+
+#     def forward(self, x):
+#         z = self.layer1(x)
+#         z = self.act1(z)
+#         z = self.layer2(z)
+#         return z
+
+net = RegressionNet(512)  
 
 x_train = torch.linspace(-10, 5, 100)
 y_train = target_function(x_train)
@@ -43,18 +63,20 @@ y_validation = target_function(x_validation)
 x_validation.unsqueeze_(1)
 y_validation.unsqueeze_(1)
 
-optimizer = torch.optim.Adam(net.parameters(), lr=0.014)  
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)  
+# optimizer =  torch.optim.AdamW(net.parameters(), lr=0.00015, weight_decay=0.001)
 
-# Sigmoid() 3930 lr=0.014
-# ReLU() 3080 lr=0.011
-for epoch_index in range(3930):
+for epoch_index in range(21930):
     optimizer.zero_grad()
     y_pred = net.forward(x_train)
     loss_value = metric(y_pred, y_train)
     loss_value.backward()
     optimizer.step()
 
+    with torch.no_grad():
+        validation_error = metric(net.forward(x_validation), y_validation)
+
     if (epoch_index + 1) % 50 == 0:
-        print(f'Epoch {epoch_index + 1}, Loss: {loss_value.item():.4f}')
+        print(f'Epoch {epoch_index + 1}, Train loss: {loss_value.item():.4f}, Validation loss: {validation_error.item():.4f}')
 
 predict(net, x_validation, y_validation)
